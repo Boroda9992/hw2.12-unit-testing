d1fdff1aa22f1f6c6f04f976b0853342925050f3
Boroda9992 <fedko.ak@gmail.com>
2024-11-29 23:27:11 +0300

    init commit

---
+++ src/main/java/pro/sky/calculator_unit_test/CalculatorUnitTestApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator_unit_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorUnitTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/calculator_unit_test/CalculatorUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator_unit_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
